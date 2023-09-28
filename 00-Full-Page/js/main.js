a98dec2167747bf111d6fa6fb5d3ad26d20d7e7b
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2023-09-28 16:32:00 +0200

    Image rotation

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -52,6 +52,44 @@ function moveImages(e) {
   const { clientWidth, clientHeight } = target;
 
   console.log(offsetX, offsetY, target, clientWidth, clientHeight);
+
+  const xPos = offsetX / clientWidth - 0.5;
+  const yPos = offsetY / clientHeight - 0.5;
+
+  const leftImages = gsap.utils.toArray(".hg__left .hg__image");
+  const rightImages = gsap.utils.toArray(".hg__right .hg__image");
+  const modifier = (index) => index * 1.2 + 0.5;
+
+  leftImages.forEach((image, index) => {
+    gsap.to(image, {
+      duration: 1.2,
+      x: xPos * 20 * modifier(index),
+      y: yPos * 30 * modifier(index),
+      //Perspective is set in the css header container
+      //This allows for rotation to happen
+      rotationY: xPos * 40,
+      rotationX: yPos * 10,
+      ease: "Power3.out",
+    });
+  });
+
+  rightImages.forEach((image, index) => {
+    gsap.to(image, {
+      duration: 1.2,
+      x: xPos * 20 * modifier(index),
+      y: -yPos * 30 * modifier(index),
+      rotationY: xPos * 40,
+      rotationX: yPos * 10,
+      ease: "Power3.out",
+    });
+  });
+
+  gsap.to(".decor__circle", {
+    duration: 1.7,
+    x: 100 * xPos,
+    y: 120 * yPos,
+    ease: "Power4.out",
+  });
 }
 
 function init() {
